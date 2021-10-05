66ce303d87fbb75bf3a19257469c3670be04d425
JerHowden <jeremiahhowden@gmail.com>
2021-10-05 14:30:00 -0500

    websocket for subscriptions completed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,39 @@ import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
+  split,
+  HttpLink,
 } from '@apollo/client'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
 
 import App from './App'
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: 'https://react.eogresources.com/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'https://react.eogresources.com/graphql',
+  options: {
+    reconnect: true,
+  },
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition'
+      && definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink,
+)
+
+const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 })
 
