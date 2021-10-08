8f42eaffc6944d99de7f889491088230029cb391
JerHowden <jeremiahhowden@gmail.com>
2021-10-08 01:34:37 -0500

    metrics chart functional wip

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import {
+  CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis,
+} from 'recharts'
+import { useAppSelector } from '../app/hooks'
 
 export default function Chart() {
+  const liveData = useAppSelector(state => state.liveData)
+  // console.log(liveData)
+
   return (
-    <div />
+    <div style={{ height: '80vh', padding: '16px' }}>
+      <ResponsiveContainer>
+        <LineChart
+          width={730}
+          height={250}
+          data={
+            Object.entries(liveData.measurements)
+              .map(measurement => ({ at: parseInt(measurement[0], 10), ...measurement[1] }))
+          }
+          margin={{
+            top: 5, right: 30, left: 20, bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3' />
+          <XAxis dataKey='at' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type='monotone' dataKey='oilTemp' stroke='#8884d8' />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
